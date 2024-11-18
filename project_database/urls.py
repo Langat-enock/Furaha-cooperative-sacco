74815b7c93c3e42a16713678c0d05a2bf87d814d
ENOCK <langatenock231@gmail.com>
2024-11-17 20:29:36 -0800

    sacco

--- project_database/urls.py
+++ project_database/urls.py
@@ -21,6 +21,13 @@ from django.urls import path
 from project_database import settings
 from sacco import views
 
+from django.conf.urls.static import static
+from django.contrib import admin
+from django.urls import path
+
+from project_database import settings
+from sacco import views
+
 urlpatterns = [
 
     path('', views.customers, name='customers'),
